4c64bc2ae9e36828deac248a230c8559c90878a0
EdwardChang0207 <thshguitar2015@gmail.com>
2025-12-06 16:19:23 +0800

    class1

---
+++ 上課檔案/Day1/input&output.py
@@ -0,0 +1,24 @@
+'''
+ctrl + a -> 全選
+ctrl + z -> 上一步
+ctrl + shift + z -> 下一步
+abc
+123
+hi
+hello
+
+terminal:
+ls -> 顯示內容
+cd -> 選擇資料匣
+TAB -> 自動完成
+../ -> 上一層
+
+''(單引號) ""(雙引號) python:一樣 Cpp(C++)
+'''
+
+print(123, end='!') #abc
+print('abc') #電腦看不懂人類的語言 -> ''(裡面都是人類的語言)
+print(123+456*2)
+print(123, 'hi', 456, sep='\n')#不同原料 -> 逗號隔開
+#分隔符號seprate -> sep(init:' ') 
+#結尾符號 end(init:'\n'換行)
